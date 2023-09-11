60b41e06e28ab93b1c2808bd9040f26c994109f0
Rachael <rachael.brady@theanalyticalmoose.com>
2023-09-10 18:20:20 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === 'W');
+    return winRecord ? winRecord.year : undefined;
+}
